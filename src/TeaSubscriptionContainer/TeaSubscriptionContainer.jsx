14613ba0cf6f2d7bce3478059ece95d7d019a987
Beverly Green <greenbev000@gmail.com>
2025-04-29 16:01:11 -0500

    set up container for tea subscriptions, passed teaSubs as a prop

---
+++ src/TeaSubscriptionContainer/TeaSubscriptionContainer.jsx
@@ -0,0 +1,20 @@
+import './TeaSubscriptionContainer.css'
+import TeaSubscriptions from '../TeaSubscriptions/TeaSubscriptions'
+
+function TeaSubs( {teaSubs}) {
+    const teaSubsData = teaSubs.map(sub => {
+    return (
+        <TeaSubscriptions
+            title={sub.attributes.title} 
+            // will have to add images to backend and seed data
+            />
+    )   
+    })
+
+    return (
+        <section className='TeaSubscriptionContainer' >
+            {teaSubs}
+        </section>
+    )
+}
+export default TeaSubs
